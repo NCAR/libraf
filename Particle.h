dd186060c3f220d5c3055f37f7e0e8c25bd83114
Chris Webster <cjw@ucar.edu>
2019-05-21 16:40:45 -0600

    Change ushort to uint16_t

--- Particle.h
+++ Particle.h
@@ -25,23 +25,23 @@ class Particle
 public:
   Particle();
 
-  long time;           // Seconds since mid-night
+  long time;			// Seconds since mid-night
   long msec;
 
-  size_t w, h;		// Particle width and height
-  size_t area;		// Total diodes shadowed by this particle
-  bool  vert_gap;	// Was there a blank slice (i.e. multiple parts)
-  bool  horiz_gap;	//   and in the hozizontal dir?
-  bool  reject;
-  bool  dofReject;	// Depth of Field rejected.  Some probes have a flag for this.
-  unsigned char edge;	// particle touched either edge
-  bool  timeReject;	// Nimbus will reject (because of bad timing bar)
+  size_t	w, h;		// Particle width and height
+  size_t	area;		// Total diodes shadowed by this particle
+  bool		vert_gap;	// Was there a blank slice (i.e. multiple parts)
+  bool		horiz_gap;	//   and in the hozizontal dir?
+  bool		reject;
+  bool		dofReject;	// Depth of Field rejected.  Some probes have a flag for this.
+  unsigned char	edge;		// particle touched either edge
+  bool		timeReject;	// Nimbus will reject (because of bad timing bar)
 
-  ushort        x1, x2;	// for particles that touch both edges.
+  uint16_t	x1, x2;	// for particles that touch both edges.
 
-  uint64_t      timeWord;	// Currently milliseconds.
-  uint32_t      deltaTime;	// Amount of time between prev & this particle
-  uint32_t      liveTime;	// Amount of time consumed by particle
+  uint64_t	timeWord;	// Currently milliseconds.
+  uint32_t	deltaTime;	// Amount of time between prev & this particle
+  uint32_t	liveTime;	// Amount of time consumed by particle
 };
 }
 
