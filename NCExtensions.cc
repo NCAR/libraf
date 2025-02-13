b046fa4fdc12dff4c2425914937b158a58812788
Chris Webster <cjw@ucar.edu>
2025-02-13 06:39:51 -0700

    White space and comments tweaks.

--- NCExtensions.cc
+++ NCExtensions.cc
@@ -1,23 +1,26 @@
 /*
 -------------------------------------------------------------------------
-OBJECT NAME:    NCExtensions.cc 
+OBJECT NAME:    NCExtensions.cc
 
-FULL NAME:     NetCDF-4 extensions 
+FULL NAME:	NetCDF-4 C++ extensions
 
-DESCRIPTION:   NetCDF-4 common functions between ncpp and aeros. Methods
-               to retrieve netCDF attributes of various data types. These 
-               will trap any exceptions that the netCDF API throws. 
-               
-               @param var the variable to get the attribute from
-               @param target the attribute name to get
-               @param output the variable to place the attribute, this is the return data
-               @param defaultValue is the value to use if the attribute does not exist
-               @returns true/false as to success of reading attribute (i.e. did it exist)
+DESCRIPTION:	NetCDF-4 common functions between ncpp and aeros. Methods
+		to retrieve netCDF attributes of various data types. These
+		will trap any exceptions that the netCDF API throws.
 
-COPYRIGHT:      University Corporation for Atmospheric Research, 1995-2025
+		@param var the variable to get the attribute from
+		@param target the attribute name to get
+		@param output the variable to place the attribute, this is the return data
+		@param defaultValue is the value to use if the attribute does not exist
+		@returns true/false as to success of reading attribute (i.e. did it exist)
+
+COPYRIGHT:      University Corporation for Atmospheric Research, 2025
 -------------------------------------------------------------------------
 */
+
 #include "NCExtensions.h"
+
+
 /* -------------------------------------------------------------------- */
 bool getStringAttribute(netCDF::NcVar& var, const char target[], std::string& output)
 {
@@ -118,6 +121,7 @@ bool getVectorOfFloatAttributes(netCDF::NcVar& var, const char target[], std::ve
   }
   return true;
 }
+
 /* -------------------------------------------------------------------- */
 bool getVectorOfDoubleAttributes(netCDF::NcVar& var, const char target[], std::vector<double>& output)
 {
--- NCExtensions.h
+++ NCExtensions.h
@@ -1,22 +1,23 @@
 /*
 -------------------------------------------------------------------------
-OBJECT NAME:    NCExtensions.h 
+OBJECT NAME:    NCExtensions.h
 
-FULL NAME:     NetCDF-4 extensions 
+FULL NAME:	NetCDF-4 C++ extensions
 
-DESCRIPTION:   NetCDF-4 common functions between ncpp and aeros. Methods
-               to retrieve netCDF attributes of various data types. These 
-	       will trap any exceptions that the netCDF API throws. 
-	       
-               @param var the variable to get the attribute from
-               @param target the attribute name to get
-               @param output the variable to place the attribute, this is the return data
-               @param defaultValue is the value to use if the attribute does not exist
-               @returns true/false as to success of reading attribute (i.e. did it exist)
+DESCRIPTION:	NetCDF-4 common functions between ncpp and aeros. Methods
+		to retrieve netCDF attributes of various data types. These
+		will trap any exceptions that the netCDF API throws.
 
-COPYRIGHT:      University Corporation for Atmospheric Research, 1995-2025
+		@param var the variable to get the attribute from
+		@param target the attribute name to get
+		@param output the variable to place the attribute, this is the return data
+		@param defaultValue is the value to use if the attribute does not exist
+		@returns true/false as to success of reading attribute (i.e. did it exist)
+
+COPYRIGHT:	University Corporation for Atmospheric Research, 2025
 -------------------------------------------------------------------------
 */
+
 #ifndef NCExtensions_H
 #define NCExtensions_H
 
